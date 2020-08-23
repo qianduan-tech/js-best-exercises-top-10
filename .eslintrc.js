401264e5bee5fdfcb599a9be6c26efa70f802714
Z8265 <zhangbo8264@163.com>
2020-08-24 00:08:16 +0800

    initial environment

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV !== 'production' ? 0 : 2,
+    'max-classes-per-file': 0,
+    'class-methods-use-this': 0,
+    'no-param-reassign': 0,
+  },
+};
